f2fa141407e1351e4cd613b291b7272fce0e9e05
ms-jois <155790046+ms-jois@users.noreply.github.com>
2025-06-17 09:09:05 -0700

    updated index.ts

--- server/src/index.ts
+++ server/src/index.ts
@@ -5,65 +5,68 @@ import cors from "cors";
 import helmet from "helmet";
 import morgan from "morgan";
 import * as dynamoose from "dynamoose";
-import serverless from "serverless-http"
-import { clerkMiddleware, createClerkClient, requireAuth } from "@clerk/express";
-/* ROUTE IMPORTS */
-import courseRoutes from "./routes/courseRoutes"
-import userClerkRoutes from "./routes/userClerkRoutes";
-import transactionRoutes from "./routes/transactionRoutes"
-import userCourseProgressRoutes from "./routes/userCourseProgressRoutes"
+import serverless from "serverless-http";
 import seed from "./seed/seedDynamodb";
+import {
+  clerkMiddleware,
+  createClerkClient,
+  requireAuth,
+} from "@clerk/express";
+/* ROUTE IMPORTS */
+import courseRoutes from "./routes/courseRoutes";
+import userClerkRoutes from "./routes/userClerkRoutes";
+import transactionRoutes from "./routes/transactionRoutes";
+import userCourseProgressRoutes from "./routes/userCourseProgressRoutes";
+
 /* CONFIGURATIONS */
 dotenv.config();
-
 const isProduction = process.env.NODE_ENV === "production";
-
-if(!isProduction){
-    dynamoose.aws.ddb.local();
+if (!isProduction) {
+  dynamoose.aws.ddb.local();
 }
 
 export const clerkClient = createClerkClient({
-    secretKey: process.env.CLERK_SECRET_KEY,
-})
+  secretKey: process.env.CLERK_SECRET_KEY,
+});
 
 const app = express();
 app.use(express.json());
 app.use(helmet());
-app.use(helmet.crossOriginResourcePolicy({policy: "cross-origin"}));
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("common"));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
-app.use(clerkMiddleware())
+app.use(clerkMiddleware());
 
 /* ROUTES */
 app.get("/", (req, res) => {
-    res.send("Hello World")
+  res.send("Hello World");
 });
 
 app.use("/courses", courseRoutes);
-app.use("/users/clerk", requireAuth() ,userClerkRoutes)
-app.use("/transactions", requireAuth(), transactionRoutes)
+app.use("/users/clerk", requireAuth(), userClerkRoutes);
+app.use("/transactions", requireAuth(), transactionRoutes);
 app.use("/users/course-progress", requireAuth(), userCourseProgressRoutes);
 
 /* SERVER */
 const port = process.env.PORT || 3000;
-if(!isProduction) {
-    app.listen(port, () => {
-        console.log(`Server running on port ${port}`);
-    });
+if (!isProduction) {
+  app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+  });
 }
 
 // aws production environment
 const serverlessApp = serverless(app);
-export const handler= async(event: any, context: any) =>{
-    if(event.action === "seed"){
-        await seed();
-        return{
-            statusCode: 200,
-            body: JSON.stringify({message: "Data seeded successfully"}),
-        };
-    }else{
-        return serverlessApp(event,context);
-    }
-}
+export const handler = async (event: any, context: any) => {
+  if (event.action === "seed") {
+    await seed();
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ message: "Data seeded successfully" }),
+    };
+  } else {
+    return serverlessApp(event, context);
+  }
+};
